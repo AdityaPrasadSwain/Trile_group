2cfc26114f15aad150a697b7e79effc30b3e0cca
Adityaps52 <swainaditya921@gmail.com>
2024-10-12 09:40:44 +0530

    mod

--- ma1/form.php
+++ ma1/form.php
@@ -8,7 +8,7 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-    <title>Hello, world!</title>
+    <title>Registation</title>
     <style>
         .container{
             height: 80%;
@@ -19,12 +19,6 @@
   </head>
   <body>
     <center><h1>Well come to my php form</h1></center>
-
-    <!-- Optional JavaScript; choose one of the two! -->
-
-    <!-- Option 1: Bootstrap Bundle with Popper -->
-    
-    <!-- Option 2: Separate Popper and Bootstrap JS -->
      <div class="container">
     <form class="row g-3" action="#" method="post">
     <div class="row g-3">
@@ -80,10 +74,6 @@
   </div>
 </form>
 </div>
-    <!--
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous"></script>
-    -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
 
   </body>
@@ -118,10 +108,10 @@ if($_POST['submit']){
     $query = "INSERT INTO moon VALUES ('$fname','$lname','$email','$password','$address','$city','$state','$zip')";
     $data = mysqli_query($conn,$query);
     if($data){
-        echo "Data inserted successfull........";
+        echo "<center>Data inserted successfull........</center>";
     }
     else{
-        echo "Data insertion failed........";
+        echo "<center>Data insertion failed........</center>";
     }
 }
 ?>
